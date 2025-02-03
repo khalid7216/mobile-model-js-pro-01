38e461ae4fc52ff769bd599301948404643c3c6c
khalid7216 <156210757+khalid7216@users.noreply.github.com>
2025-02-03 06:08:57 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+let mobileModel = prompt("Enter your favorite mobile model: ");
+let modelLength = mobileModel.length;
+document.write("Your favorite mobile model: " + mobileModel + "<br>");
+document.write("Length of the model name: " + modelLength + " characters.");
